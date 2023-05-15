9444a63c6abc489ef389bf1398d05065a1ae7413
dithiane <elenamena.colorado@gmail.com>
2023-05-14 19:33:56 -0600

    Minor changes

--- server.js
+++ server.js
@@ -118,6 +118,6 @@ app.get("/api/player", (req, res) => {
   }
 });
 
-app.listen(8000, () => {
-  console.log(`Listening on 8000`);
+app.listen(4000, () => {
+  console.log(`Listening on 4000`);
 });
